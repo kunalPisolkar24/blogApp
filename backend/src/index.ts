63eb3a175e40faca64cadfcff100237d92d01492
Kunal Pisolkar <kpisolkar24@gmail.com>
2025-05-19 06:40:56 +0000

    added environment variables

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,7 +9,11 @@ const app = new Hono<{
   Bindings: {
     DATABASE_URL: string,
     JWT_SECRET: string,
-    DATABASE_URL_MIGRATE: string
+    DATABASE_URL_MIGRATE: string,
+    UPSTASH_REDIS_REST_URL: string,
+    UPSTASH_REDIS_REST_TOKEN: string,
+    RAILWAY_CONSUMER_WAKEUP_URL: string,
+    RAILWAY_WAKEUP_SECRET: string
   };
 }>();
 
